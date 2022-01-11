99ac614d683f1e1154432e2f0e16e75561971c9c
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-01-11 19:31:52 +0100

    migrations 2

--- pen/models.py
+++ pen/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Memo(models.Model):
@@ -6,3 +7,5 @@ class Memo(models.Model):
     text = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=9, decimal_places=2)
+    added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
