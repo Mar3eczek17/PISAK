6a98c491b8738e5050b6288be1d269732a9bf110
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-01-18 11:04:20 +0100

    clear text area

--- pen/static/pen/js/script.js
+++ pen/static/pen/js/script.js
@@ -5,10 +5,10 @@ window.addEventListener('load', function(){
 let i = -1;
 
 function a() {
-    i=(i+1)%36;
+    i=(i+1)%37;
     j=i-1;
     if (i==0) {
-        j=35;
+        j=36;
     }
     document.getElementById(i).className = "highlight";
     document.getElementById(j).className = "highlighted";
@@ -18,17 +18,17 @@ document.onclick= function(event) {
     if (event===undefined) event= window.event;
     var target= 'target' in event? event.target : event.srcElement;
 
-    if (i!=-1 && i!=35) {
+    if (i!=-1 && i!=35 && i!=36) {
         var text = document.getElementById('my_console').value;
         var update_text = text + document.getElementById(i).value;
         document.getElementById('my_console').value = update_text;
     }
 
-    if (i==35) {
+    if (i==35 || i==36) {
         document.getElementById('jsform').submit();
     }
 };
 
 function clearTextarea(){
-  $('#output').val("");
+  $('#my_console').val("");
 }
